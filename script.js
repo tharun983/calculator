82a422c34001a72ca5d31764ad3d034bd5a06845
Tharun <pappulat41@gmail.com>
2026-06-14 12:54:56 +0530

    now it can handle calculations

--- script.js
+++ script.js
@@ -47,6 +47,11 @@ function updateDisplay(value) {
 
 function handleDigit(digit) {
 
+    if (shouldReset) {
+        currentInput = "0";
+        shouldReset = false;
+    }
+
     if (currentInput === "0") {
         currentInput = digit;
     } else if (digit === "." && currentInput.includes(".")) {
@@ -67,3 +72,68 @@ numberButtons.forEach(function(button){
         handleDigit(button.dataset.num);
     });
 });
+
+// New State variables -------------------------------------------------------------------
+
+let firstOperand = null;
+let operator = null;
+let shouldReset = false;
+
+// Handle Operator Clicks ----------------------------------------------------------------
+
+function handleOperator(op) {
+    const current = parseFloat(currentInput);
+
+    if (firstOperand !== null && !shouldReset) {
+        const result = operate(operator,firstOperand,current);
+        updateDisplay(result);
+        currentInput = String(result);
+        firstOperand = result;
+    } else {
+        firstOperand = current;
+    }
+
+    operator = op;
+    shouldReset = true;
+}
+
+// Handle Clear ----------------------------------------------------------------------------
+
+function handleEquals() {
+
+    if (operator === null || firstOperand === null) return;
+
+    const secondOperand = parseFloat(currentInput);
+    const result = operate(operator, firstOperand, secondOperand);
+
+    updateDisplay(result);
+    currentInput = String(result);
+
+    // Reeset state so calculator is ready for new calculation
+    firstOperand = null;
+    operator = null;
+    shouldReset = true;
+}
+
+// Handle Clear ------------------------------------------------------------------------------
+
+function handleClear() {
+    currentInput = "0";
+    firstOperand = null;
+    operator = null;
+    shouldReset = false;
+    updateDisplay("0");
+}
+
+// New Event Listeners ------------------------------------------------------------------------
+
+const operatorButtons = document.querySelectorAll(".operator");
+
+operatorButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        handleOperator(button.dataset.op);
+    });
+});
+
+document.querySelector(".equals").addEventListener("click", handleEquals);
+document.querySelector(".clear").addEventListener("click", handleClear);
